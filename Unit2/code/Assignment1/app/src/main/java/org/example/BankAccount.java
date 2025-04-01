d731ea21fdd7c2a188b34ba1f9a37e9adfdc476d
Sanjay Kumar <85833739+SanjayKumar301106@users.noreply.github.com>
2025-04-01 09:39:39 +0530

    My Commit Message

--- Unit2/code/Assignment1/app/src/main/java/org/example/BankAccount.java
+++ Unit2/code/Assignment1/app/src/main/java/org/example/BankAccount.java
@@ -8,15 +8,31 @@ public class BankAccount {
     private double balance;
     
     public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
         // TODO: Initialize balance
     }
     
     public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+        balance += amount;
+        System.out.println("Deposited: " + amount + ", New Balance: " + balance);
+    
         // TODO: Add validation
     }
     
     public void withdraw(double amount) {
         // TODO: Add overdraft protection
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdrawal amount must be positive");
+        }
+        if (amount > balance) {
+            System.out.println("Insufficient funds. Withdrawal denied.");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount + ", New Balance: " + balance);
+        }
     }
     
     public double getBalance() {
